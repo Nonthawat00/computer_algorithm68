4b9e2d66cced253ec0770ca605ad52d79c551678
Nonthawat00 <frank.kung123456789@gmail.com>
2026-01-12 11:14:57 +0700

    Add files via upload

---
+++ Python/68_algorithm/week5/week5.py
@@ -0,0 +1,21 @@
+Fullname = ("Nonthawat Meesakprasert")
+StudentID = 684245005
+Gpax = 4.00
+Age = 18
+
+if(type(Fullname) == str):
+    print("Nonthawat is str")
+else:
+    print("False")
+if(type(StudentID) == complex):
+    print("684245005 is complex")
+else:
+    print("False")
+if(type(Gpax) == float):
+    print("4.00 is float")
+else:
+    print("False")
+if(type(Age) == int):
+    print("18 is int")
+else:
+    print("False")
